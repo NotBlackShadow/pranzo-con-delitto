8c49b35816863239d863532ee3321b72f4bafa9f
Sommo Luminescente <163528607+LPBGitH@users.noreply.github.com>
2026-01-29 11:22:24 +0100

    Add files via upload

---
+++ credits.php
@@ -0,0 +1,24 @@
+<main>
+    <h1>Crediti</h1>
+
+    <h2>Addetto ai sistemi informatici, digitali e web INF4</h2>
+    <h3>Team di sviluppo del sito web</h3>
+    <p>Davide Corpus</p>
+    <p>Lars Godeassi</p>
+    <p>Leonardo Pittana Blasutig</p>
+    <p>Samuele Zoia</p>
+
+    <h3>Adetti al testo del sito web</h3>
+    <p>Andrea Zucco</p>
+    <p>Daniel Nicodemo</p>
+    <p>Filippo Turra</p>
+    <p>Gioele Specogna</p>
+    <p>Manuela Maria Puleo</p>
+    <p>Matteo Curir</p>
+    <p>Mihael Slipcevic</p>
+    <p>Tommaso De Giorgio</p>
+
+    <h2>Cameriere di sala e bar</h2>
+
+    <h2>Addetto alla computer grafica</h2>
+</main>
